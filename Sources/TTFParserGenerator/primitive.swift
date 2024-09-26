64eee45e9a88a8d63a06e2c08e3fa7127fbbd03e
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-26 18:25:27 +0800

    pts

--- Sources/TTFParserGenerator/primitive.swift
+++ Sources/TTFParserGenerator/primitive.swift
@@ -7,59 +7,143 @@ struct SizeBound {
     var lower: Int?
 }
 
-struct DataType {
-    let name: String
-
+struct ComputedTypeAnnotation {
     var isFixedDecodable: Bool?
-    /// Exact encoding size
     var encodingSize: Int?
-    /// Size bound of the convex hull of reachable regions
     var sizeBound: SizeBound
+}
 
-    init(
-        name: String,
-        isFixedDecodable: Bool? = nil,
-        encodingSize: Int? = nil,
-        sizeBound: SizeBound
-    ) {
-        self.name = name
-        self.isFixedDecodable = isFixedDecodable
-        self.encodingSize = encodingSize
-        self.sizeBound = sizeBound
+struct FieldAnnotation {
+    //
+}
+
+protocol DataTypeProtocol {
+    var computedAnnotation: ComputedTypeAnnotation? { get }
+}
+
+indirect enum DataType {
+    case primitive(PrimitiveType)
+    case array(ArrayType)
+    case structType(StructType)
+
+    func isArray() -> Bool {
+        switch self {
+        case .array:
+            return true
+        default:
+            return false
+        }
     }
 }
 
-struct FixedDecodableProperty {
-    let encodingSize: Int
+struct PrimitiveType: DataTypeProtocol {
+    let name: String
+    let computedAnnotation: ComputedTypeAnnotation?
+
+    init(
+        name: String,
+        encodingSize: Int
+    ) {
+        self.name = name
+        self.computedAnnotation = ComputedTypeAnnotation(
+            isFixedDecodable: true,
+            encodingSize: encodingSize,
+            sizeBound: SizeBound(upper: encodingSize, lower: encodingSize)
+        )
+    }
 }
 
-/// Init a primitive type
-private func pt(_ name: String, encodingSize: Int) -> DataType {
-    DataType(
-        name: name,
-        isFixedDecodable: true,
-        encodingSize: encodingSize,
-        sizeBound: SizeBound(upper: encodingSize, lower: encodingSize)
-    )
+struct ArrayType: DataTypeProtocol {
+    let elementType: DataType
+    var computedAnnotation: ComputedTypeAnnotation?
+
+    init(elementType: DataType, computedAnnotation: ComputedTypeAnnotation? = nil) {
+        precondition(!elementType.isArray())
+        self.elementType = elementType
+        self.computedAnnotation = computedAnnotation
+    }
 }
 
-let PRIMITIVE_TYPES: [DataType] = [
-    pt("uint8", encodingSize: 1),
-    pt("int8", encodingSize: 1),
-    pt("uint16", encodingSize: 2),
-    pt("int16", encodingSize: 2),
-    pt("uint24", encodingSize: 3),
-    pt("uint32", encodingSize: 4),
-    pt("int32", encodingSize: 4),
-    pt("Fixed", encodingSize: 8),
-    pt("FWORD", encodingSize: 2),
-    pt("UFWORD", encodingSize: 2),
-    pt("F2DOT14", encodingSize: 2),
-    pt("LONGDATETIME", encodingSize: 8),
-    pt("Tag", encodingSize: 4),
-    pt("Offset8", encodingSize: 1),
-    pt("Offset16", encodingSize: 2),
-    pt("Offset24", encodingSize: 3),
-    pt("Offset32", encodingSize: 4),
-    pt("Version16Dot16", encodingSize: 4),
-]
+struct StructType: DataTypeProtocol {
+    let name: String
+    var computedAnnotation: ComputedTypeAnnotation?
+    var fields: [StructField]
+
+    init(
+        name: String,
+        fields: [StructField]
+    ) {
+        self.name = name
+        self.computedAnnotation = nil
+        self.fields = fields
+    }
+}
+
+struct StructField {
+    let name: String
+    var type: DataTypeProtocol
+    let annotation: FieldAnnotation?
+
+    init(
+        name: String,
+        type: DataTypeProtocol,
+        annotation: FieldAnnotation? = nil
+    ) {
+        self.name = name
+        self.type = type
+        self.annotation = annotation
+    }
+}
+
+enum pts {
+    static let uint8 = pt("uint8", 1)
+    static let int8 = pt("int8", 1)
+    static let uint16 = pt("uint16", 2)
+    static let int16 = pt("int16", 2)
+    static let uint24 = pt("uint24", 3)
+    static let uint32 = pt("uint32", 4)
+    static let int32 = pt("int32", 4)
+    static let Fixed = pt("Fixed", 8)
+    static let FWORD = pt("FWORD", 2)
+    static let UFWORD = pt("UFWORD", 2)
+    static let F2DOT14 = pt("F2DOT14", 2)
+    static let LONGDATETIME = pt("LONGDATETIME", 8)
+    static let Tag = pt("Tag", 4)
+    static let Offset8 = pt("Offset8", 1)
+    static let Offset16 = pt("Offset16", 2)
+    static let Offset24 = pt("Offset24", 3)
+    static let Offset32 = pt("Offset32", 4)
+    static let Version16Dot16 = pt("Version16Dot16", 4)
+
+    static let allCases = [
+        uint8,
+        int8,
+        uint16,
+        int16,
+        uint24,
+        uint32,
+        int32,
+        Fixed,
+        FWORD,
+        UFWORD,
+        F2DOT14,
+        LONGDATETIME,
+        Tag,
+        Offset8,
+        Offset16,
+        Offset24,
+        Offset32,
+        Version16Dot16,
+    ]
+
+    /// Init a primitive type
+    private static func pt(
+        _ name: String,
+        _ encodingSize: Int
+    ) -> PrimitiveType {
+        PrimitiveType(
+            name: name,
+            encodingSize: encodingSize
+        )
+    }
+}
