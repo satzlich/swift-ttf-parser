9d1dc1e67961e5dc9b457a9d8c7155f82c949698
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-28 09:47:02 +0800

    pol

--- Tests/TTFParserTests/data-types/DataTypeTests.swift
+++ Tests/TTFParserTests/data-types/DataTypeTests.swift
@@ -5,68 +5,83 @@
 import XCTest
 
 final class DataTypeTests: XCTestCase {
-    func testUInt24() {
-        XCTAssertEqual(UInt24.encodingWidth, 3)
-        XCTAssertEqual(UInt24.decode([0x01, 0x02, 0x03, 0x04]).rawValue, 0x010203)
-    }
+    // MARK: - F2DOT14
 
     func testF2DOT14() {
         let eps: Float = 0.001 / 16384
 
         // 0x7fff -> 1.999939
         do {
-            let f = F2DOT14.decode([0x7F, 0xFF])
-            XCTAssertEqual(f.floatValue, 1.999939)
+            let value = F2DOT14.decode([0x7F, 0xFF])
+            XCTAssertEqual(value.floatValue, 1.999939)
         }
 
         // 0x7000 -> 1.75
         do {
-            let f = F2DOT14.decode([0x70, 0x00])
-            XCTAssertEqual(f.floatValue, 1.75)
+            let value = F2DOT14.decode([0x70, 0x00])
+            XCTAssertEqual(value.floatValue, 1.75)
         }
 
         // 0x0001 -> 0.000061
         do {
-            let f = F2DOT14.decode([0x00, 0x01])
-            XCTAssertEqual(f.floatValue, 0.000061, accuracy: eps)
+            let value = F2DOT14.decode([0x00, 0x01])
+            XCTAssertEqual(value.floatValue, 0.000061, accuracy: eps)
         }
 
         // 0x0000 -> 0.0
         do {
-            let f = F2DOT14.decode([0x00, 0x00])
-            XCTAssertEqual(f.floatValue, 0.0)
+            let value = F2DOT14.decode([0x00, 0x00])
+            XCTAssertEqual(value.floatValue, 0.0)
         }
 
         // 0xffff -> -0.000061
         do {
-            let f = F2DOT14.decode([0xFF, 0xFF])
-            XCTAssertEqual(f.floatValue, -0.000061, accuracy: eps)
+            let value = F2DOT14.decode([0xFF, 0xFF])
+            XCTAssertEqual(value.floatValue, -0.000061, accuracy: eps)
         }
 
         // 0x8000 -> -2.0
         do {
-            let f = F2DOT14.decode([0x80, 0x00])
-            XCTAssertEqual(f.floatValue, -2.0)
+            let value = F2DOT14.decode([0x80, 0x00])
+            XCTAssertEqual(value.floatValue, -2.0)
         }
     }
+
+    // MARK: - Offset24
 
     func testOffset24() {
         XCTAssertEqual(Offset24.encodingWidth, 3)
 
-        do {
-            let offset = Offset24.decode([0x01, 0x02, 0x03, 0x04])
-            XCTAssertEqual(offset.rawValue, 0x010203)
-            XCTAssertEqual(offset.isNull, false)
-        }
-
+        // null
         do {
             let offset = Offset24.decode([0x00, 0x00, 0x00, 0x00])
             XCTAssertEqual(offset.rawValue, 0x000000)
             XCTAssertEqual(offset.isNull, true)
         }
+
+        // not null
+        do {
+            let offset = Offset24.decode([0x01, 0x02, 0x03, 0x04])
+            XCTAssertEqual(offset.rawValue, 0x010203)
+            XCTAssertEqual(offset.isNull, false)
+        }
     }
 
+    // MARK: - Version16Dot16
+
     func testVersion16Dot16() {
-        XCTAssertEqual(Version16Dot16.decode([0x01, 0x02, 0x03, 0x04]).rawValue, 0x0102_0304)
+        let version = Version16Dot16.decode([0x01, 0x02, 0x03, 0x04])
+        XCTAssertEqual(version.rawValue, 0x0102_0304)
+    }
+
+    // MARK: - UInt24
+
+    func testUInt24() {
+        XCTAssertEqual(UInt24.encodingWidth, 3)
+
+        do {
+            let value = UInt24.decode([0x01, 0x02, 0x03, 0x04])
+            XCTAssertEqual(value.rawValue, 0x010203)
+        }
     }
 }
