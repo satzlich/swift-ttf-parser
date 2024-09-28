e9524d5be0ffbb2ecbb2538315341b16ae48ce0e
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-28 11:11:01 +0800

    pol

--- Sources/TTFParser/protocols/FixedDecodable.swift
+++ Sources/TTFParser/protocols/FixedDecodable.swift
@@ -2,15 +2,26 @@
 
 // MARK: - FixedDecodable
 
-/// A type that uses fixed-width encoding.
-///
-/// - Warning: Internal use only.
+/**
+ A type that uses fixed-width encoding.
+
+ - Warning: Internal use only.
+ */
 protocol FixedDecodable {
-    /// The number of bytes used to encode the value.
+    /**
+     Number of bytes used to encode the value.
+     */
     static var encodingWidth: Int { get }
 
-    /// Reconstruct the binary representation from the bytes in `data`.
-    ///
-    /// Validation over the semantic value is not performed.
-    static func decode(_ data: UnsafePointer<UInt8>) -> Self
+    /**
+     Reconstructs a value from the first ``encodingWidth`` bytes.
+     
+     - Parameter bytes: The bytes to read from
+     
+     - Returns: The reconstructed value
+
+     - Note: Only bit pattern reconstruction is carried out; **no validation**
+     over the semantic value.
+     */
+    static func decode(_ bytes: UnsafePointer<UInt8>) -> Self
 }
