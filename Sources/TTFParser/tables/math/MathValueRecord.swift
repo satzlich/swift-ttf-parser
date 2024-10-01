65dc1bd4b8280a4a31202431707c2381920c3da6
Lie Yan <robyn.lie.yan@outlook.com>
2024-10-01 19:56:15 +0800

    add math value record

---
+++ Sources/TTFParser/tables/math/MathValueRecord.swift
@@ -0,0 +1,34 @@
+// Copyright 2024 Lie Yan
+
+// MARK: - MathValueRecord
+
+struct MathValueRecord: FixedDecodable {
+    /**
+     The X or Y value in design units.
+     */
+    public var value: FWORD
+
+    /**
+     Offset to the device table, from the beginning of parent table.
+     May be NULL.
+     */
+    public var deviceOffset: Offset16
+
+    private enum Offsets {
+        static let value = 0
+        static let deviceOffset = value + FWORD.encodingWidth
+    }
+
+    public init(_ bytes: UnsafePointer<UInt8>) {
+        self.value = FWORD.decode(bytes + Offsets.value)
+        self.deviceOffset = Offset16.decode(bytes + Offsets.deviceOffset)
+    }
+
+    static let encodingWidth =
+        FWORD.encodingWidth +
+        Offset16.encodingWidth
+
+    static func decode(_ bytes: UnsafePointer<UInt8>) -> MathValueRecord {
+        MathValueRecord(bytes)
+    }
+}
