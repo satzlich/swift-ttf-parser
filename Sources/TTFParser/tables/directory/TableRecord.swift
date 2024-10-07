2a62ad36412e436b00734530580f60d06f7e2825
Lie Yan <robyn.lie.yan@outlook.com>
2024-10-07 10:15:06 +0800

    table dir

---
+++ Sources/TTFParser/tables/directory/TableRecord.swift
@@ -0,0 +1,28 @@
+// Copyright 2024 Lie Yan
+
+struct TableRecord: FixedDecodable {
+    public let tableTag: Tag
+    public let checksum: UInt32
+    public let offset: Offset32
+    public let length: UInt32
+
+    private enum Offsets {
+        static let tableTag = 0
+        static let checksum = tableTag + Tag.encodingWidth
+        static let offset = checksum + UInt32.encodingWidth
+        static let length = offset + Offset32.encodingWidth
+    }
+
+    private init(_ bytes: UnsafePointer<UInt8>) {
+        self.tableTag = Tag.decode(bytes + Offsets.tableTag)
+        self.checksum = UInt32.decode(bytes + Offsets.checksum)
+        self.offset = Offset32.decode(bytes + Offsets.offset)
+        self.length = UInt32.decode(bytes + Offsets.length)
+    }
+
+    static var encodingWidth: Int = Offsets.length + UInt32.encodingWidth
+
+    static func decode(_ bytes: UnsafePointer<UInt8>) -> TableRecord {
+        return TableRecord(bytes)
+    }
+}
